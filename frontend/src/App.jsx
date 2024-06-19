be1c8d4b82afc5ae01fea4015c61b385f79d3031
jayman779 <jadon.vanyo@gmail.com>
2024-06-19 11:39:25 -0400

    feat: established the navigation and pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,44 @@
-import React from "react"
+// Navigate between different pages using react router dom
 
+import react from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+
+// Clear tokens from local storage and return user to the login page
+function Logout() {
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
+
+// Clear any old lingering tokens from local storage and return user to the register page
+function RegisterAndLogout() {
+  localStorage.clear()
+  return <Register />
+}
+
+// Specify routes to navigate between
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/" 
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
