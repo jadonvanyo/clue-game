7962baab319893dc89b993a6f4735b36cd15aae3
jayman779 <jadon.vanyo@gmail.com>
2024-06-25 11:34:42 -0400

    chore: updated dependencies to remove navigate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 // Navigate between different pages using react router dom
 
 import { useContext } from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Home from './pages/Home'
