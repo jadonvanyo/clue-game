b31aac6d37a4c77cf16bd391d0d0d67ba8a2f139
jayman779 <jadon.vanyo@gmail.com>
2024-07-15 14:34:04 -0400

    fix: load .env into settings to fix key not found

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,10 +13,13 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load environment variables from .env file
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
