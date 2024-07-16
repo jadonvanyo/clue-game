afc5a9381862cb4d442a67623fdaacbe6c5703a4
jayman779 <jadon.vanyo@gmail.com>
2024-07-16 15:56:56 -0400

    fix: adjusted ports and dockerfile for production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,11 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    watch: {
-      usePolling: true,
-    },
     host: true, // needed for the Docker Container port mapping to work
-    strictPort: true,
-    port: 5173,
+    port: 3000,
   }
 })
